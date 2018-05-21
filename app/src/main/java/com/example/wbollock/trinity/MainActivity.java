de4e22dc64d1fa3f9b925879f47ad46b7c0d069a
Will Bollock <wbollock@gmail.com>
2018-05-21 14:47:42 -0400

    added Soul and Body

--- app/src/main/java/com/example/wbollock/trinity/MainActivity.java
+++ app/src/main/java/com/example/wbollock/trinity/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void goToMind(View view){
         Intent i = new Intent(this, Mind.class); // Mind is activity we want to launch "onClick"
+        startActivity(i);
+    }
+    public void goToBody(View view){
+        Intent i = new Intent(this, Body.class); // Body is activity we want to launch "onClick"
+        startActivity(i);
+    }
+    public void goToSoul(View view){
+        Intent i = new Intent(this, Soul.class); // Soul is activity we want to launch "onClick"
         startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/wbollock/trinity/Soul.java
@@ -0,0 +1,13 @@
+package com.example.wbollock.trinity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Soul extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soul);
+    }
+
+}
