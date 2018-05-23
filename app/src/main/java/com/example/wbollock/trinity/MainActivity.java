997a81eb8ded61a0ba7a48adb19e4fa9d78a6279
Will Bollock <wbollock@gmail.com>
2018-05-23 14:54:19 -0400

    working on startActivityforResult

--- app/src/main/java/com/example/wbollock/trinity/MainActivity.java
+++ app/src/main/java/com/example/wbollock/trinity/MainActivity.java
@@ -4,21 +4,31 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView trinityValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        trinityValue = (TextView) findViewById(R.id.trinityValue); // makes an object of trinity value
         Intent intent = new Intent(this, MainActivity.class);
         startService(intent); // starting the service for the Intent
+
+        String trinityPoints = trinityValue.getText().toString();
+        intent.putExtra("trinityPoints",trinityPoints);
     }
     public void goToMind(View view){
         Intent i = new Intent(this, Mind.class); // Mind is activity we want to launch "onClick"
-        startActivity(i);
+        startActivityForResult(i, 0);
     }
     public void goToBody(View view){
         Intent i = new Intent(this, Body.class); // Body is activity we want to launch "onClick"
@@ -28,4 +38,16 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, Soul.class); // Soul is activity we want to launch "onClick"
         startActivity(i);
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED)
+        return;
+
+    // get data here
+        if ()
+    }
+
+
 }
--- app/src/main/java/com/example/wbollock/trinity/Mind.java
+++ app/src/main/java/com/example/wbollock/trinity/Mind.java
@@ -1,15 +1,37 @@
 package com.example.wbollock.trinity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Mind extends AppCompatActivity{
+public class Mind extends AppCompatActivity {
 
+    TextView mindText;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mind);
-    }
+
+        Bundle trinityData = getIntent().getExtras();
+        if (trinityData == null)
+            return; // if trinityData is blank, take care of it
+
+        String trinityPoints = trinityData.getString("trinityPoints");
+
+        mindText = (TextView) findViewById(R.id.mindText);
+        String mindValue = mindText.getText().toString();
+        int myNum = 0;
+
+
+        try {
+            myNum = Integer.parseInt(mindValue);
+        } catch (NumberFormatException nfe) {
+            Toast.makeText(this, "could not parse", Toast.LENGTH_SHORT).show();
+        }
+
+    } // end of oncreate
+
 
 }
 
